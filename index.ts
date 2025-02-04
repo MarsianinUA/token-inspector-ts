65e80602e45d129ed08af5c7585943783d07ff11
Denis <62204087+MarsianinUA@users.noreply.github.com>
2025-02-04 23:07:02 +0200

    index.ts
    Example added

--- index.ts
+++ index.ts
@@ -8,16 +8,16 @@ const TOKEN_ADDRESS: string = process.env.TOKEN_ADDRESS || '';
 const RPC_URL: string = process.env.RPC_URL || '';
 
 if(!TOKEN_ADDRESS || !RPC_URL){
-    throw new Error('TOKEN_ADDRESS or RPC_URL are empty')
+    throw new Error('TOKEN_ADDRESS or RPC_URL are empty');
 }
 
 (async () => {
     const provider = new ethers.JsonRpcProvider(RPC_URL);
     const contract = new ethers.Contract(TOKEN_ADDRESS, ABI, provider);
     const symbol: string = await contract.symbol();
-    const decimals: string = await contract.decimals();
+    const decimals: number = await contract.decimals();
 
-    console.log(`Token address ${TOKEN_ADDRESS}`);    
+    console.log(`Token address: ${TOKEN_ADDRESS}`);    
     console.log(`Symbol: ${symbol}`);
     console.log(`Decimals: ${decimals}`);
 })();
