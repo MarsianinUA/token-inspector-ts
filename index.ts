bb779de9ce3c65631a840ddba5fcc4b35c7ae9b4
MarsianinUA <denis.stoyan21.job@gmail.com>
2025-02-04 23:39:57 +0200

    line brake

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 import { ethers } from 'ethers';
+import * as dotenv from 'dotenv';
 import ABI from './abis/erc-20.abi.json';
 
-const TOKEN_ADDRESS: string = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
-const RPC_URL: string = 'https://eth.llamarpc.com';
+dotenv.config();
+
+const TOKEN_ADDRESS: string = process.env.TOKEN_ADDRESS || '';
+const RPC_URL: string = process.env.RPC_URL || '';
+
+if(!TOKEN_ADDRESS || !RPC_URL){
+    throw new Error('TOKEN_ADDRESS or RPC_URL are empty');
+}
 
 (async () => {
     const provider = new ethers.JsonRpcProvider(RPC_URL);
     const contract = new ethers.Contract(TOKEN_ADDRESS, ABI, provider);
     const symbol: string = await contract.symbol();
+    const decimals: number = await contract.decimals();
 
-    console.log(symbol);
+    console.log(`Token address: ${TOKEN_ADDRESS}`);    
+    console.log(`Symbol: ${symbol}`);
+    console.log(`Decimals: ${decimals}`);
 })();
